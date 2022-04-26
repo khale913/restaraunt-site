fa64a6f3a006aa557c0a1e520bc7d8e028e23514
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-04-26 14:58:26 -0600

    work on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
         {toggleMenu && (
           <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
             <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => { setToggleMenu(false) }}></MdOutlineRestaurantMenu>
-            <ul className='app__navbar-smallscreen-links'>
+            <ul className='app__navbar-smallscreen_links'>
               <li className='p__opensans'><a href="#home">Home</a></li>
               <li className='p__opensans'><a href="#about">About</a></li>
               <li className='p__opensans'><a href="#menu">Menu</a></li>
@@ -46,3 +46,5 @@ const Navbar = () => {
   )
 }
 export default Navbar;
+
+//48:13
